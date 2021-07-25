aa985ff1c79b4199723f9cb8e19fde183757a63d
sean <seanim0920@gmail.com>
2021-07-24 23:45:20 -0700

    set up webpack and tested live reloading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-const App = (props) => {
+const App = (props) => (
     <div className="react-div">
         {props.name}
     </div>
-}
+)
 
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+ 
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+  },
+};
