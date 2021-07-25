5a83e532538f83b5c618cdd7b3c5ad5371f9c870
sean <seanim0920@gmail.com>
2021-07-24 21:16:33 -0700

    added react-dom added css file added test files to display a react component

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = (props) => {
+    <div className="react-div">
+        {props.name}
+    </div>
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App name="Test App Prop"/>, document.getElementById('root')); //swaps out the root div with our own custom component
