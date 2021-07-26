59489c8d234b5fb3218dd8697380d89637a691af
sean <seanim0920@gmail.com>
2021-07-25 22:01:33 -0700

    finally, a list with dynamic heights. turns out we can use "100%" as a width for the dynamic list.

--- src/DynamicallySizedList.js
+++ src/DynamicallySizedList.js
@@ -1,58 +1,68 @@
-import React from 'react';
+import React, { useRef, useEffect, useState, useCallback, memo } from 'react';
 import './App.css';
 import { VariableSizeList as List, areEqual } from 'react-window'
 import { useWindowSize } from "./getCurrentWindowSize";
 
-const DynamicallySizedList = ({ items, onAction, containerHeight, containerWidth }) => { //layout only. we may be able to calculate width and height from in here.
-    const listRef = React.useRef();
-    const rowSizes = {};
+const DynamicallySizedList = ({ items, onAction, containerHeight }) => { //layout only. we may be able to calculate width and height from in here.
+    const listRef = useRef();
+    const sizeMap = React.useRef({});
 
-    React.useEffect(() => {
-        console.log(rowSizes);
-        listRef.current.resetAfterIndex(0); //refresh list sizes cache
-    }, [items, rowSizes]);
+    useEffect(()=>{console.log("finished setting sizemap")}, [sizeMap])
 
-    const setRowSize = ({ index, size }) => {
-        rowSizes[index] = size;
-    }
+    const getSize = React.useCallback(index => sizeMap.current[index] || 100, []);
+ 
+    const setSize = React.useCallback((index, size) => {
+        sizeMap.current[index] = size;
+        console.log("setting size map ", Date.now());
+        
+        listRef.current.resetAfterIndex(index); //may be able to collect them all and use a timeout function for debouncing
+      }, []);
 
     return (
-        <List
-            height={containerHeight}
-            width={containerWidth}
-            itemCount={items.length}
-            itemSize={index => 100}//rowSizes.length > index ? rowSizes[index] : 100}
-            ref={listRef}
-        >
-            {
-                ({ index, style }) => <Row
-                item={items[index]}
-                index={index}
-                style={style}
-                onAction={onAction}
-                setRowSize={setRowSize}
-                />
-            }
-        </List>
+        items.length > 0 ?
+        <div>
+            <List
+                height={containerHeight}
+                width={"100%"}
+                itemCount={items.length}
+                itemSize={getSize}
+                ref={listRef}
+            >
+                {
+                    ({ index, style }) => <Row
+                        item={items[index]}
+                        index={index}
+                        style={style}
+                        onAction={onAction}
+                        setRowSize={setSize}
+                    />
+                }
+            </List>
+        </div>
+        : null //placeholder
     );
 };
 
-const Row = React.memo(({ item, setRowSize, onAction, index, style }) => {
-    const root = React.useRef();
+const Row = memo(({ item, setRowSize, onAction, index, style }) => {
+    const root = useRef();
     const [windowWidth] = useWindowSize();
 
-    React.useEffect(() => {
+    useEffect(() => {
+        console.log("I am gay ", root.current.getBoundingClientRect().height);
         setRowSize(index, root.current.getBoundingClientRect().height);
     }, [windowWidth]);
 
     return (
         <div
-            onClick={() => onAction(index)}
-            className={"card"}
             style={style}
-            ref={root}
         >
-            {item.label} is {item.isActive ? 'active' : 'inactive'}
+            <div
+                onClick={() => onAction(index)}
+                className={"card"}
+                ref={root}
+            >
+                {item.label} is {item.isActive ? 'active' : 'inactive'}
+            </div>
         </div>
     );
 }, areEqual);
