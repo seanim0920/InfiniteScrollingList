1c27b48679604d6bd6314b8ac73b3268405a26d9
sean <seanim0920@gmail.com>
2021-07-29 15:36:09 -0700

    no message

--- src/listComponents/ResponsiveListRow.js
+++ src/listComponents/ResponsiveListRow.js
@@ -1,22 +1,30 @@
-import React, { useRef, useEffect, memo } from 'react';
+import React, { useRef, useLayoutEffect, useEffect, useState, memo } from 'react';
 import { areEqual } from 'react-window'
 import { useWindowSize } from "helperFunctions/getCurrentWindowSize";
 
-export default memo(function ResponsiveListRow({ itemExists, setRowSize, index, style, children }) {
+export default memo(function ResponsiveListRow({ item, setRowSize, index, style, children }) {
     const cellContainer = useRef();
     const [windowWidth] = useWindowSize();
+    const [transitionEnabled, setTransitionEnabled] = useState(false);
 
     useEffect(() => {
-        if (itemExists) setRowSize(index, cellContainer.current.getBoundingClientRect().height); //need to tell the list to resize us and send our new height
-    }, [windowWidth, itemExists]);
+        if (item) setRowSize(index, cellContainer.current.getBoundingClientRect().height); //need to tell the list to resize us and send our new height
+    }, [windowWidth, item]);
+    
+    useLayoutEffect(() => {
+        setTransitionEnabled(false); //need to tell the list to resize us and send our new height
+    }, [item]);
+    
+    useLayoutEffect(() => {
+        setTransitionEnabled(true); //need to tell the list to resize us and send our new height
+    }, [transitionEnabled]);
 
     return ( //the outer div applies a placeholder style, while the inner div measures the exact size of the cell
         <div
-            style={style}
-            className={"Item"}
+            style={{...style, transition: transitionEnabled ? "all 300ms ease-in-out" : "none"}}
         >
             {
-                itemExists ?
+                item ?
                     <div
                         ref={cellContainer}
                     >
