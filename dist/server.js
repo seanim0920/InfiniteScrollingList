912c6796531dce59e53559333de9d23c196f6bf7
sean <seanim0920@gmail.com>
2021-07-30 03:25:14 -0700

    no message

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
+// the __dirname is the current directory from where the script is running   
 app.use(express.static(__dirname));
 
 // send the user to index html page inspite of the url
