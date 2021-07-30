8e82e1751d4c9ca9f95b01fb3e410b1b0591919f
sean <seanim0920@gmail.com>
2021-07-30 01:33:35 -0700

    trying to add login

--- dist/server.js
+++ dist/server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+const config = {
+  authRequired: true,
+  auth0Logout: true,
+  secret: '5R6YsGKhqs02uGdSsHuPv0wjncy603vz-j6Y_ffRVhkEiWNLUT3QpDYEQ2pUhdCU',
+  baseURL: 'https://infinitescrollinglist.herokuapp.com/',
+  clientID: 'jywbGrzbqFNlZ1H90I33WCBXkUaMKESF',
+  issuerBaseURL: 'https://dev-bsator0c.us.auth0.com'
+};
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.use(auth(config));
+
+app.get('*', requiresAuth(), (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'app.html'));
 });
 
 app.listen(port);
