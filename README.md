050cb84b2f6f5e6eab9d92199542668006f04970
sean <seanim0920@gmail.com>
2021-07-30 04:10:12 -0700

    no message

--- README.md
+++
@@ -1,2 +0,0 @@
-# InfiniteScrollingList
-UXE Exercise
--- dist/server.js
+++ dist/server.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
+const { auth } = require('express-openid-connect');
 
-// the __dirname is the current directory from where the script is running   
-app.use(express.static(__dirname));
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'ZKx9RYZHJbibHJm1vVrmr1HbcwShZHAlRT2NXaJuAg3iPVZAR4mLekilH7Nni02y',
+  baseURL: 'https://infinitescrollinglist.herokuapp.com',
+  clientID: 'ZxIL6jgOmmp2zceJIBjBkcbEpf7g7eqx',
+  issuerBaseURL: 'https://dry-wind-8885.us.auth0.com'
+};
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
-
-app.listen(port);
